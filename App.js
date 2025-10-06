b481f319eebd29607df7a1c6390dbb7942e14935
Kassii <kassidy.satterfield@outlook.com>
2025-10-06 02:48:05 -0500

    fix

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ProductList from './ProductList.js';
-
+function name(user) {
+  return user.First + ' ' + user.Last;
+}
 
 export default function App() {
+  const user = {
+    First: 'Ton',
+    Last: 'trinh'
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Shopping Bunnii</Text>
+      <Text style={styles.textStyles}>Shopping Bunnii, {formatName(user)}</Text>
       <ProductList />
     </View>
   );
@@ -19,4 +26,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontSize: 50
+  }
+
 });
